2b2f93b624c0631f8c8048a1d5a7d8476ef7cc0e
petrosdawit <petros_dawit@brown.edu>
2016-07-03 18:03:18 -0400

    hi

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$call = $_POST['call'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Message: $message";
+$recipient = "petros_dawit@brown.edu";
+$subject = "New Message From Personal Website Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
