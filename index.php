823435c1671566a859de26b2ec7135f5a6ddf20e
petrosdawit <petros_dawit@brown.edu>
2016-07-05 21:21:27 -0400

    hi

--- index.php
+++ index.php
@@ -1,13 +1,33 @@
 <?php
+
+//get parameters
 $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $call = $_POST['call'];
 $message = $_POST['message'];
-$formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Message: $message";
-$recipient = "petros_dawit@brown.edu";
-$subject = "New Message From Personal Website Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader)
-echo "Thank You!";
+$submit = $_POST['submit'];
+
+
+if($submit){
+
+    $from = 'From: petontheinter.net'."\r\n"; 
+    $to = 'cloudsblack84@gmail.com'; 
+    $subject = "Message from petontheinter.net";
+    $body = "".
+        "From: ".$name."\n".
+        "E-mail: ".$email."\n".
+        "Phone: ".$phone."\n".
+        "Call: ".$call."\n".
+        "Message: ".$message."\n";
+
+    //Send email
+    if(mail($to, $subject, $body, $from)){
+        echo '<p>Your message has been sent!</p>';
+    }
+    else{
+        die('<p>Something went wrong, go back and try again!</p>'); 
+    }
+}
+
 ?>
